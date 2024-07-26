e1fc898f26f944eed32a2c32852a6b9a818baacc
Janvi <jrajyaguru11@gmail.com>
2024-07-26 11:12:26 +0530

    Updated submodule changes

--- app/src/main/java/com/example/quiz/ResultActivity.java
+++ app/src/main/java/com/example/quiz/ResultActivity.java
@@ -6,17 +6,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class ResultActivity extends AppCompatActivity {
-    TextView tv, tv2, tv3;
+    TextView tv,tv2,tv3;
     Button btnRestart;
 
+    public ResultActivity() {
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result);
-
+        super.onCreate( savedInstanceState );
+        EdgeToEdge.enable( this );
+        setContentView( R.layout.activity_result );
+        ViewCompat.setOnApplyWindowInsetsListener( findViewById( R.id.main ), (v, insets) -> {
+            Insets systemBars = insets.getInsets( WindowInsetsCompat.Type.systemBars() );
+            v.setPadding( systemBars.left, systemBars.top, systemBars.right, systemBars.bottom );
+            return insets;
+        } );
         tv = findViewById(R.id.tvres);
         tv2 = findViewById(R.id.tvres2);
         tv3 = findViewById(R.id.tvres3);
